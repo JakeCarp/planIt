1e4ff4e19d17996ade06f9d6d24786002608aab4
jacob carpenter <jakecarpenter407@gmail.com>
2021-12-15 12:22:36 -0700

    rebuild

---
+++ planIt/client/assets/AboutPage.e0a49056.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.8fadddac.js";import{o,b as t,d as s}from"./vendor.21ee2239.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function d(_,i,p,u,f,h){return o(),t("div",c,r)}var b=e(a,[["render",d]]);export{b as default};
